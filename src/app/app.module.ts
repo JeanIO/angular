4d717958e6763f4158ec1490396967be4ef5eba2
John <13274598721@163.com>
2019-08-30 18:42:05 +0800

    create angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { CartService } from './cart.service';
+import { CartListComponent } from './cart-list/cart-list.component';
+import { ShippingComponentComponent } from './shipping-component/shipping-component.component';
+import { CheckCartComponent } from './check-cart/check-cart.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailComponent},
+      { path: 'cart', component: CartListComponent },
+      { path: 'shipping', component: ShippingComponentComponent },
+      { path: 'check-cart', component: CheckCartComponent }
+    ])
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    CartListComponent,
+    ShippingComponentComponent,
+    CheckCartComponent
+  ],
+  bootstrap: [ AppComponent ],
+  providers: [CartService]
 })
 export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
